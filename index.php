9785ea2a39b062a641c2a290557e8e5d54964c5d
cindysx89e <cindyxie89@gmail.com>
2014-01-26 15:58:11 -0500

    php include

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php include('include/header.php'); ?>
+			<!-- Fixed navbar -->
+			<div class="navbar navbar-default navbar-fixed-top" role="navigation">
+				<div class="container">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#">Selected Resources</a>
+					</div>
+					<div class="navbar-collapse collapse" style="height: 1px;">
+						<ul class="nav navbar-nav">
+							<li class="active"><a href="index.html">Home</a></li>
+							<li><a href="tutorials.html">Tutorials</a></li>
+							<li><a href="tools.html">Tools</a></li>
+							<li><a href="articles.html">Articles</a></li>
+							<li><a href="blogs.html">Blogs</a></li>
+							<li><a href="reference.html">Reference</a></li>
+						</ul>
+					</div><!--/.nav-collapse -->
+				</div>
+			</div>
+
+			<!-- Begin page content -->
+			<div class="container">
+				<div class="page-header">
+					<h1>Hi there!</h1>
+				</div>
+				<p class="lead">Welcome to Lara's Preferred Resources for budding web designers.</p>
+				<p>I'm always sending clients the same list of go-to resources - might as well put 'em in a website! It takes me way too long to write emails anyway.</p>
+				<p>Feel free to <a href="mailto:lara@notlaura.com">get in touch</a> if you think I'm missing anything!</p>
+				<hr />
+				<small>P.S. for you GA folk, this is basically the list on Schoologeezy.</small>
+			</div>
+		</div>
+
+<?php include('include/footer.php'); ?>
